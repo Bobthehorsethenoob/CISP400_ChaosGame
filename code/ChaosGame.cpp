18a19842de9df97c7d8490abe32699c697166b27
IsmaelEP <100651802+IsmaelEP@users.noreply.github.com>
2024-04-18 02:30:06 -0700

    Update ChaosGame.cpp

--- code/ChaosGame.cpp
+++ code/ChaosGame.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <cmath>
+#include "button.h"
 //comment3
 // Make code easier to type with "using namespace"
 using namespace sf;
@@ -21,7 +23,13 @@ int main()
         Color color;
     };
 
+    Color mainColor(rand() % 230 + 26, rand() % 230 + 26, rand() % 230 + 26);
 
+    int btnClick = 0;
+    int btn2Click = 0;
+
+    int colorWheel = 0;
+    int shapeWheel = 0;
 
     vector<Vector2f> vertices;
     vector<ColoredPoint> points;
@@ -38,7 +46,14 @@ int main()
     instructions.setCharacterSize(64);
     instructions.setFillColor(Color::Red);
     
-    
+    Button btn("Theme Rainbow", { 350, 50 }, 20, sf::Color::Red, sf::Color::Black);
+    btn.setPosition({ 100, 100 });
+    btn.setFont(instructionsFont);
+
+
+    Button btn2("Shape Circle", { 350, 50 }, 20, sf::Color::Red, sf::Color::Black);
+    btn2.setPosition({ 500, 100 });
+    btn2.setFont(instructionsFont);
         
 	while (window.isOpen())
 	{
@@ -57,7 +72,7 @@ int main()
             }
             if (event.type == sf::Event::MouseButtonPressed)
             {
-                if (event.mouseButton.button == sf::Mouse::Left)
+                if (event.mouseButton.button == sf::Mouse::Left && !btn.isMouseOver(window) && !btn2.isMouseOver(window))
                 {
                     std::cout << "the left button was pressed" << std::endl;
                     std::cout << "mouse x: " << event.mouseButton.x << std::endl;
@@ -69,13 +84,80 @@ int main()
                     }
                     else if(points.size() == 0)
                     {
-                        Color c(rand() % 230+26, rand() % 230+26, rand() % 230+26);
-                        points.push_back({ Vector2f(event.mouseButton.x, event.mouseButton.y), c});
-                        ///fourth click
-                        ///push back to points vector
+                        Color c(rand() % 230 + 26, rand() % 230 + 26, rand() % 230 + 26);
+                        
+                        Color r(rand() % 230 + 26, 0, 0);
+
+                        Color g(0,  rand() % 230 + 26, 0);
+
+                        Color b(0 , 0, rand() % 230 + 26);
+
+
+
+                        if (colorWheel == 0)
+                        {
+                            points.push_back({ Vector2f(event.mouseButton.x, event.mouseButton.y), c });
+                        }
+                        else if (colorWheel == 1)
+                        {
+                            points.push_back({ Vector2f(event.mouseButton.x, event.mouseButton.y), r });
+                        }
+                        else if (colorWheel == 2)
+                        {
+                            points.push_back({ Vector2f(event.mouseButton.x, event.mouseButton.y), g });
+                        }
+                        else if (colorWheel == 3)
+                        {
+                            points.push_back({ Vector2f(event.mouseButton.x, event.mouseButton.y), b });
+                        }
+                    }
+                }
+
+                if (event.mouseButton.button == sf::Mouse::Left && btn.isMouseOver(window))
+                {
+                    btnClick++;
+                    colorWheel++;
+
+                    cout << "Color Theme Clicked" << endl;
+
+                    if(btnClick == 1) { btn.settxt("Theme Red");
+                    mainColor = Color::Red;
+                    }
+                    if (btnClick == 2) { btn.settxt("Theme Green");
+                    mainColor = Color::Green;
+                    }
+                    if (btnClick == 3) { btn.settxt("Theme Blue");
+                    mainColor = Color::Blue;
+                    }
+                    if (btnClick == 4) 
+                    { 
+                        btn.settxt("Theme Rainbow"); 
+                        btnClick = 0;
+                        colorWheel = 0;
+                    }
+                }
+
+                if (event.mouseButton.button == sf::Mouse::Left && btn2.isMouseOver(window))
+                {
+                    btn2Click++;
+                    shapeWheel++;
+
+
+                    cout << "Shape Picker Clicked" << endl;
+
+                    if (btn2Click == 1) { btn2.settxt("Shape Triagnle"); }
+                    if (btn2Click == 2) { btn2.settxt("Shape Square"); }
+                    if (btn2Click == 3) { btn2.settxt("Shape Pentagon"); }
+                    if (btn2Click == 4)
+                    {
+                        btn2.settxt("Shape Circle");
+                        btn2Click = 0;
+                        shapeWheel = 0;
                     }
                 }
             }
+            
+            
         }
         if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
@@ -90,12 +172,47 @@ int main()
         if (points.size() > 0)
         {
             int randomNumber;
+            
      //       randomNumber = rand() % 3;
             for (int i = 0; i < max(int(log2(points.size())), 1); i++)
             {
+               
                 randomNumber = rand() % 3;
-                Color c(rand() % 230+26, rand() % 230+26, rand() % 230+26);
-                points.push_back({ Vector2f((points.back().location.x + vertices.at(randomNumber).x) / 2, (points.back().location.y + vertices.at(randomNumber).y) / 2),c });
+                Color c(rand() % 230 + 26, rand() % 230 + 26, rand() % 230 + 26);
+
+                
+                Color r(rand() % 230 + 26, 0, 255 % (i + 1) / 10);
+
+                Color g(255 % (i + 1) / 10, rand() % 230 + 26, 0);
+
+                Color b(255 % (i + 1)/10, 0, rand() % 230 + 26);
+
+                Color mainColor(rand() % 230 + 26, rand() % 230 + 26, rand() % 230 + 26);
+                
+                if (colorWheel == 0)
+                {
+                    points.push_back({ Vector2f((points.back().location.x + vertices.at(randomNumber).x) / 2, (points.back().location.y + vertices.at(randomNumber).y) / 2),c });
+                    
+                }
+                else if (colorWheel == 1)
+                {
+                    points.push_back({ Vector2f((points.back().location.x + vertices.at(randomNumber).x) / 2, (points.back().location.y + vertices.at(randomNumber).y) / 2),r });
+                    
+                }
+                else if (colorWheel == 2)
+                {
+                    points.push_back({ Vector2f((points.back().location.x + vertices.at(randomNumber).x) / 2, (points.back().location.y + vertices.at(randomNumber).y) / 2),g });
+                    
+                }
+                else if (colorWheel == 3)
+                {
+                    points.push_back({ Vector2f((points.back().location.x + vertices.at(randomNumber).x) / 2, (points.back().location.y + vertices.at(randomNumber).y) / 2),b });
+                    
+                }
+                
+                
+                
+                
             }
             cout << points.size() << endl;
             ///generate more point(s)
@@ -113,7 +230,9 @@ int main()
         
         if (points.size() == 0)
         {
-            
+            btn.drawTo(window);
+            btn2.drawTo(window);
+
             instructions.setPosition(10.f, 10.f);
             if (vertices.size() < 3)
                 {
@@ -127,17 +246,75 @@ int main()
             }
         for(int i = 0; i < vertices.size(); i++)
         {
-            RectangleShape rect(Vector2f(10,10));
-            rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
-            rect.setFillColor(Color::Blue);
-            window.draw(rect);
+            
+
+            CircleShape circ(4);
+
+
+            if (shapeWheel == 0)
+            {
+                
+                circ.setPosition(Vector2f(vertices[i].x, vertices[i].y));
+                circ.setFillColor(mainColor);
+                window.draw(circ);
+            }
+            else if (shapeWheel == 2)
+            {
+                CircleShape sqr(4, 4);
+                sqr.setPosition(Vector2f(vertices[i].x, vertices[i].y));
+                sqr.setFillColor(mainColor);
+                window.draw(sqr);
+            }
+            else if (shapeWheel == 1)
+            {
+                CircleShape tri(4, 3);
+                tri.setPosition(Vector2f(vertices[i].x, vertices[i].y));
+                tri.setFillColor(mainColor);
+                window.draw(tri);
+            }
+            else if (shapeWheel == 3)
+            {
+                CircleShape pent(4, 5);
+                pent.setPosition(Vector2f(vertices[i].x, vertices[i].y));
+                pent.setFillColor(mainColor);
+                window.draw(pent);
+            }
+
         }
         for (int i = 0; i < points.size(); i++)
         {
-            CircleShape circ(4);
-            circ.setPosition(Vector2f(points[i].location.x, points[i].location.y));
-            circ.setFillColor(points[i].color);
-            window.draw(circ);
+            CircleShape circle(4);
+
+            
+            if (shapeWheel == 0)
+            {
+                circle.setPosition(Vector2f(points[i].location.x, points[i].location.y));
+                circle.setFillColor(points[i].color);
+                window.draw(circle);
+            }
+            else if (shapeWheel == 1)
+            {
+                CircleShape triangle(4, 3);
+                triangle.setPosition(Vector2f(points[i].location.x, points[i].location.y));
+                triangle.setFillColor(points[i].color);
+                window.draw(triangle);
+            }
+            else if (shapeWheel == 2)
+            {
+                CircleShape square(4, 4);
+                square.setPosition(Vector2f(points[i].location.x, points[i].location.y));
+                square.setFillColor(points[i].color);
+                window.draw(square);
+            }
+            else if (shapeWheel == 3)
+            {
+                CircleShape pentagon(4, 5);
+                pentagon.setPosition(Vector2f(points[i].location.x, points[i].location.y));
+                pentagon.setFillColor(points[i].color);
+                window.draw(pentagon);
+            }
+
+            
         }
         window.display();
     }
